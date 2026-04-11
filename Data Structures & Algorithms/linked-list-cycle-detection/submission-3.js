b85bd79bda0a294a34d6854e23e1669bf03ffd2e
cherfi mohammed abdelwahab <cherfi.m.abdelwahab@gmail.com>
2026-04-11 08:33:16 +0100

    Add: linked-list-cycle-detection - submission-3

---
+++ Data Structures & Algorithms/linked-list-cycle-detection/submission-3.js
@@ -0,0 +1,58 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     constructor(val = 0, next = null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {ListNode} head
+     * @return {boolean}
+     */
+    hasCycle(head) {
+        let curr = head;
+        let breadcrumbs = new Set();
+        /* initial solution
+        let index = -1;
+
+        while(curr !== null) {
+            breadcrumbs.set(curr,(breadcrumbs.get(curr) || 0) + 1);
+           if(breadcrumbs.get(curr) > 1) {
+            curr.next = null;
+            index = 1;
+           }
+           curr = curr.next;
+           
+        }
+        return  index == 1 ? true : false;
+        */
+
+     // second solution
+        while(curr !== null) {
+            if(breadcrumbs.has(curr)) {
+                return true;
+            }
+            breadcrumbs.add(curr);
+            curr = curr.next;
+        }
+        return false;
+        
+       /* // trying the fast and slow method
+        let slow = head;
+        let fast = head;
+        while(fast !== null && fast.next !== null ) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if( slow === fast) {
+                return true;
+            }
+            
+        }
+        return false;
+        */
+    }
+}
